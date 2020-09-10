5881d2691c69ad76c2e50b9668610bbb960f12d5
Matous <bralor92@email.cz>
2020-09-10 10:32:31 +0200

    lekce12-fixes: 'scraper.py' added #98

--- scraper.py
+++ scraper.py
@@ -1,28 +1,26 @@
 #!/usr/bin/python3.8
 """Lekce #12 - Uvod do programovani, web scraping"""
-
-import sys
 import csv
+import sys
 
 import requests
 from bs4 import BeautifulSoup
 
 
 def hlavni() -> None:
-    odpoved_serveru = vytvor_pozadavek(url).text
+    odpoved = vytvor_pozadavek(url)
+    naparsovane = zpracuj_pozadavek(odpoved.text)
 
-    if odpoved_serveru:
-        naparsovana_odp = zpracuj_pozadavek(odpoved_serveru)
-        hraci = hledej_hrace(naparsovana_odp)
-        udaje = [zpracuj_udaj(hrac) for hrac in hraci]
+    trs = hledej_hrace(naparsovane)
+    data = [zpracuj_udaj(tr) for tr in trs]
 
-    if uloz_csv(udaje, soubor):
-        print(f"ULOZENO DO: {soubor}")
+    if zapis_csv(soubor, data):
+        print(f"ULOZENE DO: {soubor}")
     else:
-        print(f"NELZE ULOZIT!")
+        print("NELZE ULOZIT!")
 
 
-def vytvor_pozadavek(url: str) -> str:
+def vytvor_pozadavek(url: str) -> requests.models.Response:
     with requests.Session() as se:
         return se.get(url)
 
@@ -36,7 +34,7 @@ def hledej_hrace(data: BeautifulSoup) -> list:
     return table.find_all("tr")[1:]
 
 
-def zpracuj_udaj(tr: str) -> dict:
+def zpracuj_udaj(tr: BeautifulSoup) -> dict:
     poradi = tr.find_all("td")[0].text
     jmeno = tr.find_all("td")[2].text
     body = tr.find_all("td")[3].text
@@ -54,28 +52,29 @@ def zpracuj_udaj(tr: str) -> dict:
     }
 
 
-def uloz_csv(data: dict, jmeno: str) -> None:
+def zapis_csv(jmeno: str, data: list) -> None:
     with open(jmeno, "w", newline="") as csv_s:
         zahlavi = data[0].keys()
-        writer = csv.DictWriter(csv_s, fieldnames=zahlavi)
+        zapisovac = csv.DictWriter(csv_s, fieldnames=zahlavi)
 
-        writer.writeheader()
+        zapisovac.writeheader()
         for index, _ in enumerate(data):
-            writer.writerow(
-                {
-                    "poradi": data[index]["poradi"],
-                    "jmeno": data[index]["jmeno"],
-                    "body": data[index]["body"],
-                    "celkem": data[index]["celkem"],
-                    "vitezstvi": data[index]["vitezstvi"],
-                    "uspesnost": data[index]["uspesnost"],
-                }
+            zapisovac.writerow(
+            {
+                "poradi": data[index]["poradi"],
+                "jmeno": data[index]["jmeno"],
+                "body": data[index]["body"],
+                "celkem": data[index]["celkem"],
+                "vitezstvi": data[index]["vitezstvi"],
+                "uspesnost": data[index]["uspesnost"],
+            }
             )
         return True
 
-
+            
 if __name__ == "__main__":
     url = sys.argv[1]
     soubor = sys.argv[2]
     hlavni()
     # http://heroes3.cz/hraci/index.php?page=1&order=&razeni=DESC
+
