c6da8a921c5ecc5a932fad0fe59cf4ec4e3ebec3
Matous <bralor92@email.cz>
2020-11-12 10:22:08 +0100

    lekce02-fixes:
    destinatio_part2 - file was removed
    destinatio_p2.py - original file renamed #122

--- destinatio_p2.py
+++ destinatio_p2.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
-""" Lekce #2 - Uvod do programovani, 2/2 Destinatio """
+"""Lekce #2 - Uvod do programovani, 2/2 Destinatio """
 
-SEZNAM_MEST = ("Praha", "Viden", "Olomouc", "Svitavy", "Zlin", "Ostrava")
-SEZNAM_CEN = (150, 200, 120, 120, 100, 180)
+MESTA = ["Praha", "Viden", "Olomouc", "Svitavy", "Zlin", "Ostrava"]
+CENY = (150, 200, 120, 120, 100, 180)
+SLEVY = ("Olomouc", "Ostrava")
 ODDELOVAC = "=" * 35
+AKT_ROK = 2020
 
-print(ODDELOVAC)
-print("Vitejte u nasi aplikace Destinatio!")
+print("VITEJTE U NASI APLIKACE DESTINATIO!")
 print(ODDELOVAC)
 print(
-    """
+"""
 1 - Praha   | 150
 2 - Viden   | 200
 3 - Olomouc | 120
@@ -21,47 +22,32 @@ print(
 
 print(ODDELOVAC)
 
-# I. KROK:
-# Update/doplnit zadani
-SLEVY = ("Olomouc", "Ostrava")
 
-# II. KROK:
-# Spravne cislo lokality! 1 < x < 6
-por_cislo = int(input("Vyberte cislo lokality: "))
-
-if por_cislo < 1 or por_cislo > 6:  # logical operators + conditions
-    print("Vami vybrane cislo neni v nabidce, ukoncuji")
-    exit()  # exit()
-else:
-    destinace = SEZNAM_MEST[por_cislo - 1]
-    cena = SEZNAM_CEN[por_cislo - 1]
+cislo_lokality = int(input("Vyberte cislo lokality: "))
+if cislo_lokality > 1 or cislo_lokality < 6:
+    destinace = MESTA[cislo_lokality - 1]
+    cena = CENY[cislo_lokality - 1]
     print(f"DESTINACE: {destinace}")
     print(ODDELOVAC)
+else:
+    print("Vami vybrane cislo neni v nabidce, ukoncuji")
+    exit()
 
-# III. KROK:
-# Vyresime pouziti slevy --> ANO/NE
-if destinace in SLEVY:  # membership testing
+if destinace in SLEVY:
     cena_po_sleve = 0.75 * cena
 else:
     cena_po_sleve = cena
 
-# IV. KROK:
-# jmeno + prijmeni obsahuje jen pismena
 jmeno = input("JMENO: ")
 prijmeni = input("PRIJMENI: ")
-
-if jmeno.isalpha() and prijmeni.isalpha():  # metody stringu
+if jmeno.isalpha() and prijmeni.isalpha():
     print(f"JMENO: {jmeno}, PRIJMENI: {prijmeni}")
     print(ODDELOVAC)
 else:
     print("Jmeno a prijmeni musi obsahovat pouze pismena, ukoncuji")
     exit()
 
-# V. KROK:
-# aktualni rok - datum narozeni --> > 18
-AKT_ROK = 2020
 vek = int(input("ROK NAROZENI: "))
-
 if (AKT_ROK - vek) >= 18:
     print(f"Pokracuji...")
     print(ODDELOVAC)
@@ -69,8 +55,6 @@ else:
     print("Nase sluzby mohou vyuzivat pouze osoby starsi 18 let, ukoncuji")
     exit()
 
-# VI. KROK:
-# Spravny email
 email = input("EMAIL: ")
 if "@" in email:
     print("Email v poradku, pokracuji...")
@@ -79,17 +63,14 @@ else:
     print("Nepodporovany format emailu, ukoncuji")
     exit()
 
-# VII. KROK:
-# Heslo obsahuje jak cisla, tak pismena + delka?
 heslo = input("HESLO: ")
-
-if len(heslo) >= 8 and not heslo.isalpha() and not heslo.isnumeric():
+if len(heslo) >= 8 or heslo.isalpha() and heslo.isnumeric():
     print("Heslo v poradku")
-    print(ODDELOVAC)
     print("UZIVATEL: " + jmeno)
     print("DESTINACE: " + destinace)
     print("CENA(cil:" + destinace + "): " + str(cena_po_sleve))
     print(f"Jizdenku posleme na Vasi emailovou adresu: {email}")
+    print(ODDELOVAC)
 else:
     print(
         """Tvoje heslo je spatne zadane:
@@ -97,3 +78,4 @@ else:
 	2. Alespon 8 znaku dlouhe,
     """
     )
+
