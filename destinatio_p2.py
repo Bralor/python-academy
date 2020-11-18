102cda0d8dc109ac3a6c73388cb39155af82f9e6
Matous <bralor92@email.cz>
2020-11-18 12:44:44 +0100

    lekce02-fixes: destinatio_p2.py updated (minor updates) #127

--- destinatio_p2.py
+++ destinatio_p2.py
@@ -23,28 +23,31 @@ print(
 print(ODDELOVAC)
 
 cislo_lokality = int(input("VYBERTE CISLO LOKALITY: "))
-if cislo_lokality > 1 or cislo_lokality < 6:
+
+if 0 < cislo_lokality < len(MESTA):
     destinace = MESTA[cislo_lokality - 1]
     cena = CENY[cislo_lokality - 1]
     print(f"DESTINACE: {destinace}")
     print(ODDELOVAC)
 else:
     print("VAMI VYBRANE CISLO NENI V NABIDCE, UKONCUJI..")
-    exit()
+    quit()
 
 if destinace in SLEVY:
     cena_po_sleve = 0.75 * cena
+    print("ZISKAVATE 25% SLEVU!")
 else:
     cena_po_sleve = cena
 
 jmeno = input("JMENO: ")
 prijmeni = input("PRIJMENI: ")
+
 if jmeno.isalpha() and prijmeni.isalpha():
     print(f"JMENO: {jmeno}, PRIJMENI: {prijmeni}")
     print(ODDELOVAC)
 else:
     print("JMENO A PRIJMENI MUSI OBSAHOVAT POUZE PISMENA, UKONCUJI..")
-    exit()
+    quit()
 
 vek = int(input("ROK NAROZENI: "))
 if (AKT_ROK - vek) >= 18:
@@ -52,29 +55,31 @@ if (AKT_ROK - vek) >= 18:
     print(ODDELOVAC)
 else:
     print("NASE SLUZBY MOHOU VYUZIVAT POUZE OSOBY STARSI 18 LET, UKONCUJI..")
-    exit()
+    quit()
 
 email = input("EMAIL: ")
-if "@" in email:
+
+if "@" in email and email[-3:] == ".cz":
     print("EMAIL V PORADKU, POKRACUJI..")
     print(ODDELOVAC)
 else:
     print("NEPODPOROVANY FORMAT EMAILU, UKONCUJI..")
-    exit()
+    quit()
 
 heslo = input("HESLO: ")
-if len(heslo) >= 8 or heslo.isalpha() and heslo.isnumeric():
+
+if len(heslo) >= 8 and not heslo.isalpha() and not heslo.isnumeric():
     print("HESLO V PORADKU")
     print(ODDELOVAC)
     print("DESTINACE: " + destinace)
     print("DEKUJEME,", jmeno, "JIZDENKU POSLEME NA EMAIL:", email)
     print(f"CENA (CIL: {destinace}): {cena}")
-    print(ODDELOVAC)
 else:
     print(
-        """TVOJE HESLO JE SPATNE ZADANE:
-	1. MUSI OBSAHOVAT JAK PISMENA, TAK CISLICE,
-	2. ALESPON 8 ZNAKU DLOUHE,
+      """TVOJE HESLO JE SPATNE ZADANE:
+    1. MUSI OBSAHOVAT ALESPON 8 ZNAKU
+    2. MUSI OBSAHOVAT PISMENA
+    3. MUSI OBSAHOVAT CISLICE
     """
     )
 
